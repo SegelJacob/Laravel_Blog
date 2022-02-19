9b5e847a52e5fa0eb20846de02a1e04540b025e7
segel <segel.jacob92@gmail.com>
2022-02-19 14:32:33 -0500

    Add a blog post

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,16 @@
+<!doctype html>
+
+<title>My Blog</title>
+<link rel="stylesheet" href="/app.css"/>
+
+<body>
+    <article>
+        <h1>Sourdough Basics</h1>
+
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab asperiores, aspernatur consequatur cupiditate deserunt ea facilis labore maxime mollitia obcaecati odio officia, praesentium quia reiciendis sed sequi suscipit temporibus tenetur!
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab asperiores, aspernatur consequatur cupiditate deserunt ea facilis labore maxime mollitia obcaecati odio officia, praesentium quia reiciendis sed sequi suscipit temporibus tenetur!
+        </p>
+    </article>
+
+</body>
