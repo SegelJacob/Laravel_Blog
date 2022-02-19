3bd2e3ca46ef299c04f5026dc0282961275f6c48
segel <segel.jacob92@gmail.com>
2022-02-19 15:14:02 -0500

    use slugs to dynamically load blog posts

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,34 +1,34 @@
-<!doctype html>
+    <!doctype html>
 
-<title>My Blog</title>
-<link rel="stylesheet" href="/app.css"/>
+    <title>My Blog</title>
+    <link rel="stylesheet" href="/app.css"/>
 
-<body>
-    <article>
-        <h1>Sourdough Basics</h1>
+    <body>
+        <article>
+            <h1> <a href="/basics">Sourdough Basics</a></h1>
 
-        <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab asperiores, aspernatur consequatur cupiditate deserunt ea facilis labore maxime mollitia obcaecati odio officia, praesentium quia reiciendis sed sequi suscipit temporibus tenetur!
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab asperiores, aspernatur consequatur cupiditate deserunt ea facilis labore maxime mollitia obcaecati odio officia, praesentium quia reiciendis sed sequi suscipit temporibus tenetur!
-        </p>
-    </article>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab asperiores, aspernatur consequatur cupiditate deserunt ea facilis labore maxime mollitia obcaecati odio officia, praesentium quia reiciendis sed sequi suscipit temporibus tenetur!
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab asperiores, aspernatur consequatur cupiditate deserunt ea facilis labore maxime mollitia obcaecati odio officia, praesentium quia reiciendis sed sequi suscipit temporibus tenetur!
+            </p>
+        </article>
 
-    <article>
-        <h1>How to feed your sourdough starter</h1>
+        <article>
+            <h1><a href="/post">How to feed your sourdough starter</a></h1>
 
-        <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab asperiores, aspernatur consequatur cupiditate deserunt ea facilis labore maxime mollitia obcaecati odio officia, praesentium quia reiciendis sed sequi suscipit temporibus tenetur!
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab asperiores, aspernatur consequatur cupiditate deserunt ea facilis labore maxime mollitia obcaecati odio officia, praesentium quia reiciendis sed sequi suscipit temporibus tenetur!
-        </p>
-    </article>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab asperiores, aspernatur consequatur cupiditate deserunt ea facilis labore maxime mollitia obcaecati odio officia, praesentium quia reiciendis sed sequi suscipit temporibus tenetur!
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab asperiores, aspernatur consequatur cupiditate deserunt ea facilis labore maxime mollitia obcaecati odio officia, praesentium quia reiciendis sed sequi suscipit temporibus tenetur!
+            </p>
+        </article>
 
-    <article>
-        <h1>Making your first loaf</h1>
+        <article>
+            <h1><a href="/post">Making your first loaf</a></h1>
 
-        <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab asperiores, aspernatur consequatur cupiditate deserunt ea facilis labore maxime mollitia obcaecati odio officia, praesentium quia reiciendis sed sequi suscipit temporibus tenetur!
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab asperiores, aspernatur consequatur cupiditate deserunt ea facilis labore maxime mollitia obcaecati odio officia, praesentium quia reiciendis sed sequi suscipit temporibus tenetur!
-        </p>
-    </article>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab asperiores, aspernatur consequatur cupiditate deserunt ea facilis labore maxime mollitia obcaecati odio officia, praesentium quia reiciendis sed sequi suscipit temporibus tenetur!
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab asperiores, aspernatur consequatur cupiditate deserunt ea facilis labore maxime mollitia obcaecati odio officia, praesentium quia reiciendis sed sequi suscipit temporibus tenetur!
+            </p>
+        </article>
 
-</body>
+    </body>
--- tests/Feature/BlogPostTest.php
+++ tests/Feature/BlogPostTest.php
@@ -5,7 +5,7 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
-class ExampleTest extends TestCase
+class BlogPostTest extends TestCase
 {
     /**
      * A basic test example.
@@ -14,6 +14,7 @@ class ExampleTest extends TestCase
      */
     public function test_the_application_returns_a_successful_response()
     {
+        $this->withoutExceptionHandling();
         $response = $this->get('/');
 
         $response->assertStatus(200);
