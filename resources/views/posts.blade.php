1c2b57382e409567ca3c702b3a1c143b5a401871
segel <segel.jacob92@gmail.com>
2022-02-19 14:36:25 -0500

    create multiple blog posts

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -13,4 +13,22 @@
         </p>
     </article>
 
+    <article>
+        <h1>How to feed your sourdough starter</h1>
+
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab asperiores, aspernatur consequatur cupiditate deserunt ea facilis labore maxime mollitia obcaecati odio officia, praesentium quia reiciendis sed sequi suscipit temporibus tenetur!
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab asperiores, aspernatur consequatur cupiditate deserunt ea facilis labore maxime mollitia obcaecati odio officia, praesentium quia reiciendis sed sequi suscipit temporibus tenetur!
+        </p>
+    </article>
+
+    <article>
+        <h1>Making your first loaf</h1>
+
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab asperiores, aspernatur consequatur cupiditate deserunt ea facilis labore maxime mollitia obcaecati odio officia, praesentium quia reiciendis sed sequi suscipit temporibus tenetur!
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab asperiores, aspernatur consequatur cupiditate deserunt ea facilis labore maxime mollitia obcaecati odio officia, praesentium quia reiciendis sed sequi suscipit temporibus tenetur!
+        </p>
+    </article>
+
 </body>
