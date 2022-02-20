e38bea4d2bfc45674435e4d65720ef818699cfce
segel <segel.jacob92@gmail.com>
2022-02-19 19:24:28 -0500

    use caching

--- routes/web.php
+++ routes/web.php
@@ -2,18 +2,18 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('posts');
-});
+    Route::get('/', function () {
+        return view('posts');
+    });
 
-Route::get('posts/{post}', function ($slug) {
-    $path = __DIR__ . "/../resources/posts/{$slug}.html";
+    Route::get('posts/{post}', function ($slug) {
+        $path = __DIR__ . "/../resources/posts/{$slug}.html";
 
-    if (! file_exists($path)){
-        return redirect('/');
-    }
-    $post = file_get_contents($path);
-    return view('post', [
-        'post'=> $post
-    ]);
-})->where('post','[A-z_\-]+');
+        if (! file_exists($path)){
+            return redirect('/');
+        }
+
+        $post = cache()->remember("posts.{$slug}",1200, fn() => file_get_contents($path));
+
+        return view('post', ['post'=> $post]);
+    })->where('post','[A-z_\-]+');
