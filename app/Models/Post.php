a9ff19097212bad95a2e4dddd26a5b23b3c21f0f
segel <segel.jacob92@gmail.com>
2022-02-19 19:59:54 -0500

    use filesystem

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -5,10 +5,18 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Support\Facades\File;
 
-class Post extends Model
+class Post
 {
     use HasFactory;
+
+    public static function all()
+    {
+        $files = File::files(resource_path("posts/"));
+
+        return array_map(fn($file) => $file->getContents(),$files);
+    }
 
     public static function find($slug)
     {
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -4,31 +4,9 @@
     <link rel="stylesheet" href="/app.css"/>
 
     <body>
-        <article>
-            <h1> <a href="/posts/basics">Sourdough Basics</a></h1>
-
-            <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab asperiores, aspernatur consequatur cupiditate deserunt ea facilis labore maxime mollitia obcaecati odio officia, praesentium quia reiciendis sed sequi suscipit temporibus tenetur!
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab asperiores, aspernatur consequatur cupiditate deserunt ea facilis labore maxime mollitia obcaecati odio officia, praesentium quia reiciendis sed sequi suscipit temporibus tenetur!
-            </p>
-        </article>
-
-        <article>
-            <h1><a href="/posts/feed_starter">How to feed your sourdough starter</a></h1>
-
-            <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab asperiores, aspernatur consequatur cupiditate deserunt ea facilis labore maxime mollitia obcaecati odio officia, praesentium quia reiciendis sed sequi suscipit temporibus tenetur!
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab asperiores, aspernatur consequatur cupiditate deserunt ea facilis labore maxime mollitia obcaecati odio officia, praesentium quia reiciendis sed sequi suscipit temporibus tenetur!
-            </p>
-        </article>
-
-        <article>
-            <h1><a href="/posts/first_loaf">Making your first loaf</a></h1>
-
-            <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab asperiores, aspernatur consequatur cupiditate deserunt ea facilis labore maxime mollitia obcaecati odio officia, praesentium quia reiciendis sed sequi suscipit temporibus tenetur!
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab asperiores, aspernatur consequatur cupiditate deserunt ea facilis labore maxime mollitia obcaecati odio officia, praesentium quia reiciendis sed sequi suscipit temporibus tenetur!
-            </p>
-        </article>
-
+        <?php foreach ($posts as $post)  : ?>
+            <article>
+                <?= $post; ?>
+            </article>
+        <?php endforeach; ?>
     </body>
