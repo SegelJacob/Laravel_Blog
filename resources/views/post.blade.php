b5ed99c189ef38296af46ff96ce1104bccaca895
segel <segel.jacob92@gmail.com>
2022-02-20 14:55:59 -0500

    refactor

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -5,11 +5,10 @@
 
     <body>
         <article>
-            <h1><?= /** @var post $post */
-                $post->title; ?></h1>
+            <h1>{{ $post->title }}</h1>
 
             <div>
-                <?= $post->body; ?>
+                {!! $post->body !!}
             </div>
         </article>
 
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -7,13 +7,13 @@
 <?php foreach ($posts as $post)  : ?>
 <article>
     <h1>
-        <a href="/posts/<?= $post->slug; ?>">
-            <?= $post->title; ?>
+        <a href="/posts/{{ $post->slug }}">
+            {{ $post->title }}
         </a>
     </h1>
 
     <div>
-        <?= $post->excerpt; ?>
+        {{ $post->excerpt }}
     </div>
 </article>
 <?php endforeach; ?>
