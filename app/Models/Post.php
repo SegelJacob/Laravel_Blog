572cd771f674a773b11eedb5e48024074bed821b
segel <segel.jacob92@gmail.com>
2022-02-19 21:37:03 -0500

    refactor to sort posts by date

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -37,15 +37,14 @@ class Post
                 $document->date,
                 $document->body(),
                 $document->slug,
-            ));
+            ))
+            ->sortByDesc('date');
     }
 
     public static function find($slug)
     {
-        if (!file_exists($path = resource_path("/posts/{$slug}.html"))) {
-            throw new ModelNotFoundException();
-        }
-        return cache()->remember("posts.{$slug}", 1200, fn() => file_get_contents($path));
+        return static::all()->firstWhere('slug',$slug);
+
     }
 
 }
