89b9f5153d0503a672bf249f8a9ee68a17b9cca0
segel <segel.jacob92@gmail.com>
2022-02-19 21:12:22 -0500

    use collect helper method

--- routes/web.php
+++ routes/web.php
@@ -9,16 +9,17 @@ Route::get('/', function () {
 
     $files = File::files(resource_path("posts/"));
 
-    $posts = array_map(function ($file) {
-        $document = YamlFrontMatter::parseFile($file);
-        return new Post(
-            $document->title,
-            $document->excerpt,
-            $document->date,
-            $document->body(),
-            $document->slug,
-        );
-    },$files);
+    $posts = collect($files)
+        ->map(function($file){
+            $document = YamlFrontMatter::parseFile($file);
+            return new Post(
+                $document->title,
+                $document->excerpt,
+                $document->date,
+                $document->body(),
+                $document->slug,
+            );
+        });
 
     return view('posts', [
         'posts' => $posts
