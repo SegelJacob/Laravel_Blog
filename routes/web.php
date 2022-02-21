8b71daadf922cf55d616b33c9ee8881c729e0481
segel <segel.jacob92@gmail.com>
2022-02-21 14:04:58 -0500

    eager load by default

--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
 
     return view('posts', [
-        'posts' => Post::latest('published_at')->with('category', 'author')->get()
+        'posts' => Post::latest('published_at')->get()
     ]);
 });
 
@@ -17,9 +17,9 @@ Route::get('posts/{post:slug}', fn(Post $post) => view('post', [
 ]));
 
 Route::get('categories/{category:slug}', fn(Category $category) => view('posts', [
-    'posts' => $category->posts->load(['category','author'])
+    'posts' => $category->posts
 ]));
 
 Route::get('authors/{author:username}', fn(User $author) => view('posts', [
-    'posts' => $author->posts->load(['category','author'])
+    'posts' => $author->posts
 ]));
