3469ffbffc1763004ed7eeb82ce7244f2c202681
segel <segel.jacob92@gmail.com>
2022-02-21 13:42:44 -0500

    sort by published date

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
 
     return view('posts', [
-        'posts' => Post::with('category')->get()
+        'posts' => Post::latest('published_at')->with('category')->get()
     ]);
 });
 
