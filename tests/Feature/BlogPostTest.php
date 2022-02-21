d9889ba3c683063175bfa2b518b097cf9ea37a72
segel <segel.jacob92@gmail.com>
2022-02-21 14:06:01 -0500

    remove irrelevant tests

--- tests/Feature/BlogPostTest.php
+++ tests/Feature/BlogPostTest.php
@@ -7,37 +7,4 @@ use Tests\TestCase;
 class BlogPostTest extends TestCase
 {
 
-    public function test_the_application_returns_a_successful_response_for_first_loaf_post()
-    {
-        $this->markTestSkipped();
-        $value = 'Your first loaf';
-        $response = $this->get('/posts/first_loaf');
-
-        $response->assertStatus(200);
-        $response->assertSeeText($value, $escaped = true);
-    }
-
-    public function test_the_application_returns_a_successful_response_for_feed_starter()
-    {
-        $value = 'How to feed your starter';
-        $response = $this->get('/posts/feed-starter');
-
-        $response->assertStatus(200);
-        $response->assertSeeText($value, $escaped = true);
-    }
-
-    public function test_the_application_returns_a_successful_response_for_basics()
-    {
-        $value = 'Sourdough Basics';
-        $response = $this->get('/posts/sourdough-basics');
-
-        $response->assertStatus(200);
-        $response->assertSeeText($value, $escaped = true);
-    }
-
-    public function test_the_application_returns_an_error_response_for_post_that_does_not_exist()
-    {
-        $this->get('/posts/foo')
-            ->assertStatus(404);
-    }
 }
