e24bfed44577d72a1fbca94fbab5a2dd0fb2ee3d
segel <segel.jacob92@gmail.com>
2022-02-21 20:32:50 -0500

    update routes, define categories

--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
 
     return view('posts', [
-        'posts' => Post::latest('published_at')->get()
+        'posts' => Post::latest('published_at')->get(),
+        'categories' => Category::all()
     ]);
 });
 
