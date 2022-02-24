60c7d2245abeca1935c02311e32fd5a248046a1c
segel <segel.jacob92@gmail.com>
2022-02-24 15:40:58 -0500

    update routes to use PostController

--- routes/web.php
+++ routes/web.php
@@ -1,29 +1,12 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use App\Models\Category;
-use App\Models\Post;
 use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    $posts = Post::latest();
-
-    if (request('search')) {
-        $posts
-            ->where('title', 'like', '%' . request('search') . '%')
-            ->orWhere('body', 'like', '%' . request('search') . '%');
-    }
-
-
-    return view('posts', [
-        'posts' => $posts->get(),
-        'categories' => Category::all()
-    ]);
-})->name('home');
-
-Route::get('posts/{post:slug}', fn(Post $post) => view('post', [
-    'post' => $post
-]));
+Route::get('/', [PostController::class, 'index'])->name('home');
+Route::get('posts/{post:slug}', [PostController::class, 'show']);
 
 Route::get('categories/{category:slug}', fn(Category $category) => view('posts', [
     'posts' => $category->posts,
