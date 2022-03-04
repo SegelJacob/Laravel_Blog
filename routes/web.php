46b0aa32d52f53a06eada042500d462bda69a4c0
segel <segel.jacob92@gmail.com>
2022-03-04 15:07:19 -0500

    remove clutter

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,7 @@
 <?php
 
 use App\Http\Controllers\PostController;
-use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [PostController::class, 'index'])->name('home');
 Route::get('posts/{post:slug}', [PostController::class, 'show']);
-
-Route::get('authors/{author:username}', fn(User $author) => view('posts.index', [
-    'posts' => $author->posts,
-]));
