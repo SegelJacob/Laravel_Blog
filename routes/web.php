f862d387c0443d5abd2baf769c5e3a71e6c56b34
segel <segel.jacob92@gmail.com>
2022-03-04 14:54:37 -0500

    filter by author

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [PostController::class, 'index'])->name('home');
 Route::get('posts/{post:slug}', [PostController::class, 'show']);
 
-Route::get('authors/{author:username}', fn(User $author) => view('posts', [
+Route::get('authors/{author:username}', fn(User $author) => view('posts.index', [
     'posts' => $author->posts,
 ]));
