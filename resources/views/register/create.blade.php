d9f1c362da82aec13d295a12e486319bef709a8c
segel <segel.jacob92@gmail.com>
2022-03-06 16:28:51 -0500

    add form inputs

--- resources/views/register/create.blade.php
+++ resources/views/register/create.blade.php
@@ -1,5 +1,47 @@
 <x-layout>
     <section class="px-6 py-8">
-        <h1>Hello World!</h1>
+        <main class="max-w-lg mx-auto mt-10 bg-gray-100 border border-gray-200 p-6 rounded-xl">
+            <h1 class="text-center font-bold text-xl">Register!</h1>
+            <form method="POST" action="/register" class="mt-10">
+                <div class="mb-6">
+                    <label class="block mb-2 uppercase font-bold text-xs text-gray-700" for="name">
+                        Name
+                    </label>
+                    <input class="border border-gray-400 p-2 w-full rounded-xl"
+                           type="text"
+                           name="name"
+                           id="name"
+                           required
+                    >
+                </div>
+                <div class="mb-6">
+                    <label class="block mb-2 uppercase font-bold text-xs text-gray-700" for="username">
+                        Username
+                    </label>
+                    <input class="border border-gray-400 p-2 w-full rounded-xl"
+                           type="text"
+                           name="username"
+                           id="username"
+                           required
+                    >
+                </div>
+                <div class="mb-6">
+                    <label class="block mb-2 uppercase font-bold text-xs text-gray-700" for="password">
+                        Password
+                    </label>
+                    <input class="border border-gray-400 p-2 w-full rounded-xl"
+                           type="password"
+                           name="password"
+                           id="password"
+                           required
+                    >
+                </div>
+                <div class="mb-6">
+                    <button class="bg-blue-400 text-white rounded py-2 px-4 hover:bg-blue-500">
+                        Submit
+                    </button>
+                </div>
+            </form>
+        </main>
     </section>
 </x-layout>
