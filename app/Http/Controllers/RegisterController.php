b372480b39e31eb150ace0f1e895aa4730edaafd
segel <segel.jacob92@gmail.com>
2022-03-07 15:22:59 -0500

    create user after validation

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+
 class RegisterController extends Controller
 {
     public function create()
@@ -11,11 +13,11 @@ class RegisterController extends Controller
 
     public function store()
     {
-        request()->validate([
+        User::create(request()->validate([
             'name' => ['required', 'max:255'],
             'username' => ['required', 'max:255', 'min:3'],
             'email' => ['required', 'email', 'max:255'],
             'password' => ['required', 'max:255', 'min:7'],
-        ]);
+        ]));
     }
 }
