d89716fc545df8f988573c881ad2eb206485d176
segel <segel.jacob92@gmail.com>
2022-03-10 15:31:28 -0500

    reformat

--- resources/views/register/create.blade.php
+++ resources/views/register/create.blade.php
@@ -46,6 +46,9 @@
                            value="{{ old('email') }}"
                            required
                     >
+                    @error('email')
+                    <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                    @enderror
                 </div>
                 <div class="mb-6">
                     <label class="block mb-2 uppercase font-bold text-xs text-gray-700" for="password">
