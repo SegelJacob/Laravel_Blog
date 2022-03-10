352b6b4e02ea82c30fe5f7c83777d1bd3167d310
segel <segel.jacob92@gmail.com>
2022-03-10 12:53:39 -0500

    show error validation message when registering a user

--- resources/views/register/create.blade.php
+++ resources/views/register/create.blade.php
@@ -15,6 +15,9 @@
                            id="name"
                            required
                     >
+                    @error('name')
+                    <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                    @enderror
                 </div>
                 <div class="mb-6">
                     <label class="block mb-2 uppercase font-bold text-xs text-gray-700" for="username">
