837741cac8751e44513e1c59043452169c982eee
segel <segel.jacob92@gmail.com>
2022-03-11 15:37:25 -0500

    add link for login

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -25,6 +25,7 @@
                 </form>
             @else
                 <a href="/register" class="text-xs font-bold uppercase">Register</a>
+                <a href="/login" class="ml-6 text-xs font-bold uppercase">Login</a>
             @endauth
 
             <a href="#" class="bg-blue-500 ml-3 rounded-full text-xs font-semibold text-white uppercase py-3 px-5">
