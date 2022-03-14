3346bc731ed28069e1baff7196f220b1879bbe95
segel <segel.jacob92@gmail.com>
2022-03-14 15:00:12 -0400

    add comments section

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -8,48 +8,68 @@
                     <p class="mt-4 block text-gray-400 text-xs">
                         Published
                         <time>{{ $post->created_at->diffForHumans() }}</time>
-                </p>
+                    </p>
 
-                <div class="flex items-center lg:justify-center text-sm mt-4">
-                    <img src="/images/lary-avatar.svg" alt="Lary avatar">
-                    <div class="ml-3 text-left">
-                        <h5 class="font-bold">
-                            <a href="/?author={{ $post->author->username }}">{{ $post->author->name }}</a>
-                        </h5>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-span-8">
-                <div class="hidden lg:flex justify-between mb-6">
-                    <a href="/"
-                       class="transition-colors duration-300 relative inline-flex items-center text-lg hover:text-blue-500">
-                        <svg width="22" height="22" viewBox="0 0 22 22" class="mr-2">
-                            <g fill="none" fill-rule="evenodd">
-                                <path stroke="#000" stroke-opacity=".012" stroke-width=".5" d="M21 1v20.16H.84V1z">
-                                </path>
-                                <path class="fill-current"
-                                      d="M13.854 7.224l-3.847 3.856 3.847 3.856-1.184 1.184-5.04-5.04 5.04-5.04z">
-                                </path>
-                            </g>
-                        </svg>
-
-                        Back to Posts
-                    </a>
-
-                    <div class="space-x-2">
-                        <x-category-button :category="$post->category"/>
+                    <div class="flex items-center lg:justify-center text-sm mt-4">
+                        <img src="/images/lary-avatar.svg" alt="Lary avatar">
+                        <div class="ml-3 text-left">
+                            <h5 class="font-bold">
+                                <a href="/?author={{ $post->author->username }}">{{ $post->author->name }}</a>
+                            </h5>
+                        </div>
                     </div>
                 </div>
 
-                <h1 class="font-bold text-3xl lg:text-4xl mb-10">
-                    {{ $post->title }}
-                </h1>
+                <div class="col-span-8">
+                    <div class="hidden lg:flex justify-between mb-6">
+                        <a href="/"
+                           class="transition-colors duration-300 relative inline-flex items-center text-lg hover:text-blue-500">
+                            <svg width="22" height="22" viewBox="0 0 22 22" class="mr-2">
+                                <g fill="none" fill-rule="evenodd">
+                                    <path stroke="#000" stroke-opacity=".012" stroke-width=".5" d="M21 1v20.16H.84V1z">
+                                    </path>
+                                    <path class="fill-current"
+                                          d="M13.854 7.224l-3.847 3.856 3.847 3.856-1.184 1.184-5.04-5.04 5.04-5.04z">
+                                    </path>
+                                </g>
+                            </svg>
 
-                <div class="space-y-4 lg:text-lg leading-loose">
-                    {!! $post->body !!}
+                            Back to Posts
+                        </a>
+
+                        <div class="space-x-2">
+                            <x-category-button :category="$post->category"/>
+                        </div>
+                    </div>
+
+                    <h1 class="font-bold text-3xl lg:text-4xl mb-10">
+                        {{ $post->title }}
+                    </h1>
+
+                    <div class="space-y-4 lg:text-lg leading-loose">
+                        {!! $post->body !!}
+                    </div>
                 </div>
-            </div>
+
+                <section>
+                    <article class="flex">
+                        <div>
+                            <img src="https://i.pravatar.cc/100" alt="profile avatar">
+                        </div>
+                        <div>
+                            <header>
+                                <h3 class="font-bold">John Doe</h3>
+                                <p class="text-xs"> Posted
+                                    <time>8 mins ago</time>
+                                </p>
+                            </header>
+                            <p>
+                                lorem fugiat et sapiente unde distinctio. Praesentium aliquid necessitatibus minus
+                                nesciunt. Natus qui eos enim quo. Aut eos facere ducimus maiores ut alias.
+                            </p>
+                        </div>
+                    </article>
+                </section>
             </article>
         </main>
     </section>
