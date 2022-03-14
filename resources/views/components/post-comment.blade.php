ae1fa8c15c3c8e371e1134365dfd6a2f919b4ffc
segel <segel.jacob92@gmail.com>
2022-03-14 15:07:25 -0400

    extract comment to component

---
+++ resources/views/components/post-comment.blade.php
@@ -0,0 +1,18 @@
+<article class="flex bg-gray-100 border border-gray-200 p-6 rounded-xl space-x-4">
+    <div class="flex-shrink-0">
+        <img src="https://i.pravatar.cc/60" alt="profile avatar" width="60" height="60"
+             class="rounded-xl">
+    </div>
+    <div>
+        <header class="mb-4">
+            <h3 class="font-bold">John Doe</h3>
+            <p class="text-xs"> Posted
+                <time>8 mins ago</time>
+            </p>
+        </header>
+        <p>
+            lorem fugiat et sapiente unde distinctio. Praesentium aliquid necessitatibus minus
+            nesciunt. Natus qui eos enim quo. Aut eos facere ducimus maiores ut alias.
+        </p>
+    </div>
+</article>
