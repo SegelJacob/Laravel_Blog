e62b57bb2aae21a7a3bb1520d08c53c7718ed766
segel <segel.jacob92@gmail.com>
2022-03-14 15:16:12 -0400

    refactor

--- database/migrations/2022_03_14_190829_create_comments_table.php
+++ database/migrations/2022_03_14_190829_create_comments_table.php
@@ -19,7 +19,7 @@ return new class extends Migration {
             $table->text('body');
             $table->timestamps();
 
-            $table->foreign('post_id')->references('id')->on('posts')->onDelete('cascade');
+            $table->foreign('post_id')->references('id')->on('posts')->cascadeOnDelete();
         });
     }
 
