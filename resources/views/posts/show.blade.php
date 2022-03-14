3bde659b274b5a3ac9039f9ef5099b0e773738bb
segel <segel.jacob92@gmail.com>
2022-03-14 15:05:19 -0400

    add css to comments

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -51,13 +51,14 @@
                     </div>
                 </div>
 
-                <section>
-                    <article class="flex">
-                        <div>
-                            <img src="https://i.pravatar.cc/100" alt="profile avatar">
+                <section class="col-span-8 col-start-5 mt-10">
+                    <article class="flex bg-gray-100 border border-gray-200 p-6 rounded-xl space-x-4">
+                        <div class="flex-shrink-0">
+                            <img src="https://i.pravatar.cc/60" alt="profile avatar" width="60" height="60"
+                                 class="rounded-xl">
                         </div>
                         <div>
-                            <header>
+                            <header class="mb-4">
                                 <h3 class="font-bold">John Doe</h3>
                                 <p class="text-xs"> Posted
                                     <time>8 mins ago</time>
