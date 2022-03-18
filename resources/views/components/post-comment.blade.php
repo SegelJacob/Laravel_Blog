782f7b5ead41bd65d61f8bb5bad0882e2c0039b9
segel <segel.jacob92@gmail.com>
2022-03-18 14:48:59 -0400

    make comments dynamic

--- resources/views/components/post-comment.blade.php
+++ resources/views/components/post-comment.blade.php
@@ -1,3 +1,4 @@
+@props(['comment'])
 <article class="flex bg-gray-100 border border-gray-200 p-6 rounded-xl space-x-4">
     <div class="flex-shrink-0">
         <img src="https://i.pravatar.cc/60" alt="profile avatar" width="60" height="60"
@@ -5,14 +6,13 @@
     </div>
     <div>
         <header class="mb-4">
-            <h3 class="font-bold">John Doe</h3>
-            <p class="text-xs"> Posted
-                <time>8 mins ago</time>
+            <h3 class="font-bold">{{ $comment->author->username }}</h3>
+            <p class="text-xs"> Posted at
+                <time>{{ $comment->created_at }}</time>
             </p>
         </header>
         <p>
-            lorem fugiat et sapiente unde distinctio. Praesentium aliquid necessitatibus minus
-            nesciunt. Natus qui eos enim quo. Aut eos facere ducimus maiores ut alias.
+            {{ $comment->body }}
         </p>
     </div>
 </article>
