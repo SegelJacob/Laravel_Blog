dc1849e5a9184be710b895b8b0904662efed3e8b
segel <segel.jacob92@gmail.com>
2022-03-29 15:41:21 -0400

    add comment id to url

--- resources/views/components/post-comment.blade.php
+++ resources/views/components/post-comment.blade.php
@@ -1,7 +1,7 @@
 @props(['comment'])
 <article class="flex bg-gray-100 border border-gray-200 p-6 rounded-xl space-x-4">
     <div class="flex-shrink-0">
-        <img src="https://i.pravatar.cc/60" alt="profile avatar" width="60" height="60"
+        <img src="https://i.pravatar.cc/60?u={{ $comment->id }}" alt="profile avatar" width="60" height="60"
              class="rounded-xl">
     </div>
     <div>
