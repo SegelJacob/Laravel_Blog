92a756142ec114b279a7c83f3d759e38a56f4d77
segel <segel.jacob92@gmail.com>
2022-03-30 14:46:00 -0400

    create route for posting comments

---
+++ app/Http/Controllers/PostCommentsController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+
+class PostCommentsController extends Controller
+{
+    public function store(Post $post)
+    {
+        // add comment to given post
+    }
+}
