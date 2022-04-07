77469bdac01415453b51b90a0e248374c8594e66
segel <segel.jacob92@gmail.com>
2022-04-07 16:12:02 -0400

    add ability to subscribe to newsletter

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,10 @@ use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\SessionsController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('ping', function () {
+Route::post('newsletter', function () {
+    request()->validate([
+        'email' => 'required|email',
+    ]);
 
     $mailchimp = new \MailchimpMarketing\ApiClient();
 
@@ -15,8 +18,18 @@ Route::get('ping', function () {
         'server' => 'us14'
     ]);
 
-    $response = $mailchimp->ping->get();
-    dd($response);
+    try {
+        $response = $mailchimp->lists->addListMember('ebddec0690', [
+            'email_address' => request('email'),
+            'status' => 'subscribed',
+        ]);
+    } catch (Exception $e) {
+        throw \Illuminate\Validation\ValidationException::withMessages([
+            'email' => 'This email could not be added to our email list.'
+        ]);
+    }
+
+    return redirect('/')->with('success', 'You are now signed up for our newsletter!');
 });
 
 Route::get('/', [PostController::class, 'index'])->name('home');
