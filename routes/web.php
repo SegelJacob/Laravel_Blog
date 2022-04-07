ed79492a5ef504235b7c2d1a0dd3f95439446855
segel <segel.jacob92@gmail.com>
2022-04-07 16:38:38 -0400

    refactor

--- routes/web.php
+++ routes/web.php
@@ -1,28 +1,12 @@
 <?php
 
+use App\Http\Controllers\NewsletterController;
 use App\Http\Controllers\PostCommentsController;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\SessionsController;
-use App\Services\Newsletter;
 use Illuminate\Support\Facades\Route;
-use Illuminate\Validation\ValidationException;
 
-Route::post('newsletter', function (Newsletter $newsletter) {
-    request()->validate([
-        'email' => 'required|email',
-    ]);
-    try {
-        $newsletter->subscribe(request('email'));
-
-    } catch (Exception $e) {
-        throw ValidationException::withMessages([
-            'email' => 'This email could not be added to our email list.'
-        ]);
-    }
-
-    return redirect('/')->with('success', 'You are now signed up for our newsletter!');
-});
 
 Route::get('/', [PostController::class, 'index'])->name('home');
 
@@ -31,6 +15,8 @@ Route::post('posts/{post:slug}/comments', [PostCommentsController::class, 'store
 
 Route::get('register', [RegisterController::class, 'create'])->middleware('guest');
 Route::post('register', [RegisterController::class, 'store'])->middleware('guest');
+
+Route::post('newsletter', NewsletterController::class);
 
 Route::get('login', [SessionsController::class, 'create'])->middleware('guest');
 Route::post('sessions', [SessionsController::class, 'store'])->middleware('guest');
