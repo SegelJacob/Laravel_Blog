5a4b50dc4644570f2adebe71b359103e56a1e5e4
segel <segel.jacob92@gmail.com>
2022-06-07 16:16:44 -0400

    add rule for category id

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -33,7 +33,7 @@ class PostController extends Controller
             'slug' => ['required', Rule::unique('posts', 'slug')],
             'excerpt' => 'required',
             'body' => 'required',
-            'category_id' => 'required'
+            'category_id' => ['required', Rule::exists('categories', 'id')]
         ]);
 
         $attributes['user_id'] = auth()->id();
